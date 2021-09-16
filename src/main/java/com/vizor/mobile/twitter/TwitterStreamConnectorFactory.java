7d1426c1bffeb4e6648e81963f4c99e555a95e6f
zely20@mail.ru <Civik1984>
2021-09-16 21:49:04 +0300

    add impl model

--- src/main/java/com/vizor/mobile/twitter/TwitterStreamConnectorFactory.java
+++ src/main/java/com/vizor/mobile/twitter/TwitterStreamConnectorFactory.java
@@ -2,7 +2,7 @@ package com.vizor.mobile.twitter;
 
 public class TwitterStreamConnectorFactory
 {
-    public TwitterStreamConnector createConnector(String apiKey, String secretKey)
+        public TwitterStreamConnector createConnector(String apiKey, String secretKey)
     {
         throw new UnsupportedOperationException("Unimplemented");
     }
---
+++ src/main/java/com/vizor/mobile/twitter/TwitterStreamConnectorImpl.java
@@ -0,0 +1,17 @@
+package com.vizor.mobile.twitter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class TwitterStreamConnectorImpl implements TwitterStreamConnector{
+
+    private final static String URL_STREAM = "https://api.twitter.com/2/tweets/search/stream";
+    private final static String CONTENT_TYPE = "Content-type: application/json";
+    private final static String BEARER = "Authorization: Bearer ";
+
+    @Override
+    public void listenStream(List<Rule> ruleList, Consumer<Tweet> streamConsumer) throws IOException, InterruptedException {
+
+    }
+}
